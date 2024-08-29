6c71a18c882914fdb2fecb261493c64470ceaeed
DimitriChrysafis <Dimitri.chrysafis@gmail.com>
2024-08-29 15:17:15 -0700

    improved the code
    improved the code

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/dimitrichrysafis/CLionProjects/Playground
+# Install script for directory: /Users/dimitrichrysafis/Documents/GitHub/FISH
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/dimitrichrysafis/CLionProjects/Playground/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/dimitrichrysafis/Documents/GitHub/FISH/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -2,21 +2,24 @@
 #include <cmath>
 #include <vector>
 
-const int WIDTH = 800;
-const int HEIGHT = 600;
+using namespace std;
+using namespace sf;
+
+const int width = 800;
+const int height = 600;
 const float fishScale = 0.3;
-const sf::Color BLACK = sf::Color(0, 0, 0);
-const sf::Color WHITE = sf::Color(255, 255, 255);
-const sf::Color BODY_COLOR = sf::Color(58, 124, 165);
-const sf::Color FIN_COLOR = sf::Color(129, 195, 215);
+const Color black = Color(0, 0, 0);
+const Color white = Color(255, 255, 255);
+const Color bodyColor = Color(58, 124, 165);
+const Color finColor = Color(129, 195, 215);
 
 class Chain {
 public:
-    std::vector<sf::Vector2f> joints;
-    std::vector<float> angles;
+    vector<Vector2f> joints;
+    vector<float> angles;
     float segmentLength;
 
-    Chain(sf::Vector2f origin, int numSegments, float segmentLength)
+    Chain(Vector2f origin, int numSegments, float segmentLength)
             : segmentLength(segmentLength) {
         for (int i = 0; i < numSegments; ++i) {
             joints.push_back(origin);
@@ -24,17 +27,17 @@ public:
         }
     }
 
-    void resolve(sf::Vector2f targetPos) {
+    void resolve(Vector2f targetPos) {
         joints[0] = targetPos;
         for (int i = 1; i < joints.size(); ++i) {
-            sf::Vector2f direction = joints[i] - joints[i - 1];
-            if (std::sqrt(direction.x * direction.x + direction.y * direction.y) > 0) {
-                float len = std::sqrt(direction.x * direction.x + direction.y * direction.y);
-                direction /= len; // Normalize
-                direction *= segmentLength; // Scale to segment length
+            Vector2f direction = joints[i] - joints[i - 1];
+            if (sqrt(direction.x * direction.x + direction.y * direction.y) > 0) {
+                float len = sqrt(direction.x * direction.x + direction.y * direction.y);
+                direction /= len;
+                direction *= segmentLength;
             }
             joints[i] = joints[i - 1] + direction;
-            angles[i] = std::atan2(direction.y, direction.x);
+            angles[i] = atan2(direction.y, direction.x);
         }
     }
 };
@@ -42,9 +45,9 @@ public:
 class Fish {
 public:
     Chain spine;
-    std::vector<float> bodyWidth;
+    vector<float> bodyWidth;
 
-    Fish(sf::Vector2f origin)
+    Fish(Vector2f origin)
             : spine(origin, 12, 64 * fishScale) {
         float bodyWidths[] = {34, 41, 42, 41, 38, 32, 26, 19, 16, 9};
         for (int i = 0; i < 10; ++i) {
@@ -52,144 +55,144 @@ public:
         }
     }
 
-    void resolve(sf::Vector2f mousePos) {
-        sf::Vector2f headPos = spine.joints[0];
-        sf::Vector2f direction = mousePos - headPos;
-        float len = std::sqrt(direction.x * direction.x + direction.y * direction.y);
+    void resolve(Vector2f mousePos) {
+        Vector2f headPos = spine.joints[0];
+        Vector2f direction = mousePos - headPos;
+        float len = sqrt(direction.x * direction.x + direction.y * direction.y);
         if (len > 0) {
-            direction /= len; // Normalize
+            direction /= len;
         }
-        sf::Vector2f targetPos = headPos + direction * (16 * fishScale);
+        Vector2f targetPos = headPos + direction * (16 * fishScale);
         spine.resolve(targetPos);
     }
 
-    void display(sf::RenderWindow &window) {
-        window.clear(BLACK);
+    void display(RenderWindow& window) {
+        window.clear(black);
 
-        sf::Vector2f* j = &spine.joints[0];
+        Vector2f* j = &spine.joints[0];
         float* a = &spine.angles[0];
 
-        float head_to_mid1 = relative_angle_diff(a[0], a[6]);
-        float head_to_mid2 = relative_angle_diff(a[0], a[7]);
-        float head_to_tail = head_to_mid1 + relative_angle_diff(a[6], a[11]);
+        float headToMid1 = relativeAngleDiff(a[0], a[6]);
+        float headToMid2 = relativeAngleDiff(a[0], a[7]);
+        float headToTail = headToMid1 + relativeAngleDiff(a[6], a[11]);
 
-        draw_fin(window, 3, M_PI / 3, 160 * fishScale, 64 * fishScale, a[2] - M_PI / 4);
-        draw_fin(window, 3, -M_PI / 3, 160 * fishScale, 64 * fishScale, a[2] + M_PI / 4);
-        draw_fin(window, 7, M_PI / 2, 96 * fishScale, 32 * fishScale, a[6] - M_PI / 4);
-        draw_fin(window, 7, -M_PI / 2, 96 * fishScale, 32 * fishScale, a[6] + M_PI / 4);
+        drawFin(window, 3, M_PI / 3, 160 * fishScale, 64 * fishScale, a[2] - M_PI / 4);
+        drawFin(window, 3, -M_PI / 3, 160 * fishScale, 64 * fishScale, a[2] + M_PI / 4);
+        drawFin(window, 7, M_PI / 2, 96 * fishScale, 32 * fishScale, a[6] - M_PI / 4);
+        drawFin(window, 7, -M_PI / 2, 96 * fishScale, 32 * fishScale, a[6] + M_PI / 4);
 
-        draw_caudal_fin(window, j, a, head_to_tail);
-        draw_body(window, j, a);
-        draw_dorsal_fin(window, j, a, head_to_mid1, head_to_mid2);
+        drawCaudalFin(window, j, a, headToTail);
+        drawBody(window, j, a);
+        drawDorsalFin(window, j, a, headToMid1, headToMid2);
 
-        draw_eye(window, j[0], a[0], M_PI / 2, -18 * fishScale);
-        draw_eye(window, j[0], a[0], -M_PI / 2, -18 * fishScale);
+        drawEye(window, j[0], a[0], M_PI / 2, -18 * fishScale);
+        drawEye(window, j[0], a[0], -M_PI / 2, -18 * fishScale);
 
         window.display();
     }
 
 private:
-    void draw_fin(sf::RenderWindow &window, int index, float angleOffset, float width, float height, float rotation) {
-        sf::Vector2f pos = get_pos(index, angleOffset, 0);
-        sf::RectangleShape fin(sf::Vector2f(width, height));
-        fin.setFillColor(FIN_COLOR);
+    void drawFin(RenderWindow& window, int index, float angleOffset, float width, float height, float rotation) {
+        Vector2f pos = getPos(index, angleOffset, 0);
+        RectangleShape fin(Vector2f(width, height));
+        fin.setFillColor(finColor);
         fin.setOrigin(width / 2, height / 2);
         fin.setPosition(pos);
         fin.setRotation(rotation * 180 / M_PI);
         window.draw(fin);
     }
 
-    void draw_caudal_fin(sf::RenderWindow &window, sf::Vector2f* j, float* a, float head_to_tail) {
-        std::vector<sf::Vector2f> points;
+    void drawCaudalFin(RenderWindow& window, Vector2f* j, float* a, float headToTail) {
+        vector<Vector2f> points;
         for (int i = 8; i < 12; ++i) {
-            float tail_width = 1.5f * head_to_tail * (i - 8) * (i - 8) * fishScale;
-            points.push_back(get_vertex(j[i], a[i], tail_width, -M_PI / 2));
+            float tailWidth = 1.5f * headToTail * (i - 8) * (i - 8) * fishScale;
+            points.push_back(getVertex(j[i], a[i], tailWidth, -M_PI / 2));
         }
         for (int i = 11; i >= 8; --i) {
-            float tail_width = std::max(-13.0f, std::min(13.0f, head_to_tail * 6)) * fishScale;
-            points.push_back(get_vertex(j[i], a[i], tail_width, M_PI / 2));
+            float tailWidth = max(-13.0f, min(13.0f, headToTail * 6)) * fishScale;
+            points.push_back(getVertex(j[i], a[i], tailWidth, M_PI / 2));
         }
-        draw_polygon(window, points, BODY_COLOR, 2);
+        drawPolygon(window, points, bodyColor, 2);
     }
 
-    void draw_body(sf::RenderWindow &window, sf::Vector2f* j, float* a) {
-        std::vector<sf::Vector2f> points;
+    void drawBody(RenderWindow& window, Vector2f* j, float* a) {
+        vector<Vector2f> points;
         for (int i = 0; i < 10; ++i) {
-            points.push_back(get_vertex(j[i], a[i], bodyWidth[i], M_PI / 2));
+            points.push_back(getVertex(j[i], a[i], bodyWidth[i], M_PI / 2));
         }
-        points.push_back(get_vertex(j[9], a[9], bodyWidth[9], M_PI));
+        points.push_back(getVertex(j[9], a[9], bodyWidth[9], M_PI));
         for (int i = 9; i >= 0; --i) {
-            points.push_back(get_vertex(j[i], a[i], bodyWidth[i], -M_PI / 2));
+            points.push_back(getVertex(j[i], a[i], bodyWidth[i], -M_PI / 2));
         }
-        points.push_back(get_vertex(j[0], a[0], bodyWidth[0], -M_PI / 6));
-        points.push_back(get_vertex(j[0], a[0], bodyWidth[0], 0));
-        points.push_back(get_vertex(j[0], a[0], bodyWidth[0], M_PI / 6));
-        draw_polygon(window, points, BODY_COLOR, 2);
+        points.push_back(getVertex(j[0], a[0], bodyWidth[0], -M_PI / 6));
+        points.push_back(getVertex(j[0], a[0], bodyWidth[0], 0));
+        points.push_back(getVertex(j[0], a[0], bodyWidth[0], M_PI / 6));
+        drawPolygon(window, points, bodyColor, 2);
     }
 
-    void draw_dorsal_fin(sf::RenderWindow &window, sf::Vector2f* j, float* a, float head_to_mid1, float head_to_mid2) {
-        std::vector<sf::Vector2f> points = {
+    void drawDorsalFin(RenderWindow& window, Vector2f* j, float* a, float headToMid1, float headToMid2) {
+        vector<Vector2f> points = {
                 j[4],
                 j[7],
-                sf::Vector2f(j[6].x + cos(a[6] + M_PI / 2) * head_to_mid2 * 16 * fishScale,
-                             j[6].y + sin(a[6] + M_PI / 2) * head_to_mid2 * 16 * fishScale),
-                sf::Vector2f(j[5].x + cos(a[5] + M_PI / 2) * head_to_mid1 * 16 * fishScale,
-                             j[5].y + sin(a[5] + M_PI / 2) * head_to_mid1 * 16 * fishScale),
+                Vector2f(j[6].x + cos(a[6] + M_PI / 2) * headToMid2 * 16 * fishScale,
+                         j[6].y + sin(a[6] + M_PI / 2) * headToMid2 * 16 * fishScale),
+                Vector2f(j[5].x + cos(a[5] + M_PI / 2) * headToMid1 * 16 * fishScale,
+                         j[5].y + sin(a[5] + M_PI / 2) * headToMid1 * 16 * fishScale),
                 j[4]
         };
-        draw_polygon(window, points, FIN_COLOR, 2);
+        drawPolygon(window, points, finColor, 2);
     }
 
-    void draw_eye(sf::RenderWindow &window, sf::Vector2f pos, float angle, float angleOffset, float lengthOffset) {
-        sf::Vector2f eye_pos = get_vertex(pos, angle, 24 * fishScale, angleOffset, lengthOffset);
-        sf::CircleShape eye(12 * fishScale);
-        eye.setFillColor(WHITE);
-        eye.setPosition(eye_pos);
+    void drawEye(RenderWindow& window, Vector2f pos, float angle, float angleOffset, float lengthOffset) {
+        Vector2f eyePos = getVertex(pos, angle, 24 * fishScale, angleOffset, lengthOffset);
+        CircleShape eye(12 * fishScale);
+        eye.setFillColor(white);
+        eye.setPosition(eyePos);
         window.draw(eye);
     }
 
-    sf::Vector2f get_vertex(sf::Vector2f pos, float angle, float length, float angleOffset = 0, float lengthOffset = 0) {
+    Vector2f getVertex(Vector2f pos, float angle, float length, float angleOffset = 0, float lengthOffset = 0) {
         float offsetAngle = angle + angleOffset;
         float offsetLength = length + lengthOffset;
-        return sf::Vector2f(pos.x + cos(offsetAngle) * offsetLength,
-                            pos.y + sin(offsetAngle) * offsetLength);
+        return Vector2f(pos.x + cos(offsetAngle) * offsetLength,
+                        pos.y + sin(offsetAngle) * offsetLength);
     }
 
-    sf::Vector2f get_pos(int i, float angleOffset, float lengthOffset) {
-        return get_vertex(spine.joints[i], spine.angles[i], bodyWidth[i], angleOffset, lengthOffset);
+    Vector2f getPos(int i, float angleOffset, float lengthOffset) {
+        return getVertex(spine.joints[i], spine.angles[i], bodyWidth[i], angleOffset, lengthOffset);
     }
 
-    float relative_angle_diff(float angle1, float angle2) {
+    float relativeAngleDiff(float angle1, float angle2) {
         return atan2(sin(angle2 - angle1), cos(angle2 - angle1));
     }
 
-    void draw_polygon(sf::RenderWindow &window, const std::vector<sf::Vector2f>& points, const sf::Color& color, float outlineThickness) {
-        sf::ConvexShape shape;
+    void drawPolygon(RenderWindow& window, const vector<Vector2f>& points, const Color& color, float outlineThickness) {
+        ConvexShape shape;
         shape.setPointCount(points.size());
         for (size_t i = 0; i < points.size(); ++i) {
             shape.setPoint(i, points[i]);
         }
         shape.setFillColor(color);
-        shape.setOutlineColor(WHITE);
+        shape.setOutlineColor(white);
         shape.setOutlineThickness(outlineThickness);
         window.draw(shape);
     }
 };
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Procedural Animation: Fish Following Mouse");
-    Fish fish(sf::Vector2f(WIDTH / 2, HEIGHT / 2));
+    RenderWindow window(VideoMode(width, height), "A Fish");
+    Fish fish(Vector2f(width / 2, height / 2));
 
     while (window.isOpen()) {
-        sf::Event event;
+        Event event;
         while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed)
                 window.close();
         }
 
-        fish.resolve(sf::Vector2f(sf::Mouse::getPosition(window)));
+        fish.resolve(Vector2f(Mouse::getPosition(window)));
         fish.display(window);
-        sf::sleep(sf::milliseconds(16)); // ~60 FPS
+        sleep(milliseconds(16));
     }
 
     return 0;
